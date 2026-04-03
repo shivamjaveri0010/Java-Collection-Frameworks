2cade65623568422db1462a68d4bb78267c00da7
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-04-04 00:02:02 +0530

    Corrected Exercise of Day-18

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@
 //     public static void main(String[] args) {
 //         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //         ArrayList<String> fruitList = new ArrayList<>();
-
+        
 //         try {
 //             System.out.println("Enter five fruits: ");
 
@@ -27,7 +27,7 @@
 //             }
 
 //             System.out.println("Total number of fruits: " + fruitList.size());
-
+            
 //             if (!fruitList.isEmpty()) {
 //                 System.out.println("First Fruit: " + fruitList.get(0));
 //                 System.out.println("Last Fruit: " + fruitList.get((fruitList.size() -1)));
@@ -88,7 +88,7 @@
 //         System.out.println("Even numbers are: ");
 
 //         for (int num : nums) {
-
+            
 //             //print even numbers
 //             if (num % 2 == 0) {
 //                 System.out.println(num);
@@ -103,66 +103,3 @@
 //     }
 // }
 
-/*----------Task-18----------*/
-import java.util.ArrayList;
-import java.util.Scanner;
-
-class Student {
-    String name;
-    int marks;
-
-    Student(String name, int marks) {
-        this.name = name;
-        this.marks = marks;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Student> students = new ArrayList<>();
-
-        System.out.print("Enter number of students: ");
-        int n = sc.nextInt();
-        sc.nextLine(); // clear buffer
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter name: ");
-            String name = sc.nextLine();
-
-            System.out.print("Enter marks: ");
-            int marks = sc.nextInt();
-            sc.nextLine(); // clear buffer
-
-            students.add(new Student(name, marks));
-        }
-
-        System.out.println("\nStudent Details:");
-        for (Student s : students) {
-            System.out.println("Name: " + s.name + ", Marks: " + s.marks);
-        }
-
-        Student topStudent = students.get(0);
-
-        for (Student s : students) {
-            if (s.marks > topStudent.marks) {
-                topStudent = s;
-            }
-        }
-
-        System.out.println("\nTop Student:");
-        System.out.println("Name: " + topStudent.name + ", Marks: " + topStudent.marks);
-
-        int sum = 0;
-
-        for (Student s : students) {
-            sum += s.marks;
-        }
-
-        double average = (double) sum / students.size();
-        System.out.println("\nAverage Marks: " + average);
-
-        sc.close();
-    }
-}
